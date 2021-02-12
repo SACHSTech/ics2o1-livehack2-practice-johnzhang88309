f600a3ba3976066c603e45e42b7f90e3d7d088c1
johnzhang88309 <john.zhang23@ycdsbk12.ca>
2021-02-12 15:43:33 +0000

    m

--- problem1.py
+++ problem1.py
@@ -1,1 +1,21 @@
+"""
+--------------------------------------------------------
+Name: Right angle triangle calculator
+Purpose: Calculate if a triangle is a right triangle
 
+Creator: Zhang. J 
+Date: 12/02/2021
+--------------------------------------------------------
+"""
+
+print ("***** Right angle triangle calculator*****")
+#Write out variables
+side_1 = int(input("Enter length of side 1: "))
+side_2 = int(input("Enter length of side 2: "))
+side_3 = int(input("Enter length of side 3: "))
+
+#Write out formulas and print statements
+if (side_1 ** 2 + side_2 ** 2 == side_3 ** 2) or (side_1 ** 2 + side_3 ** 2 == side_2 ** 2) or (side_2 ** 2 + side_3 ** 2 == side_1 ** 2):
+  print("It is a right triangle. ")
+else: 
+  print("It is not a right triangle. ")
--- problem2.py
+++ problem2.py
@@ -1,1 +1,23 @@
+"""
+--------------------------------------------------------
+Name: Vacation calculator
+Purpose: Calculating a vacation
 
+Creator: Zhang. J 
+Date: 12/02/2021
+--------------------------------------------------------
+"""
+
+print("***** Vacation Calculator *****")
+
+#write out variables
+mark = int(input("Please enter your average in school: "))
+earnings_before_summer = int(input("Please enter your earnings before the summer: "))
+
+#write out formulas and print statements
+if mark >= 80 and earnings_before_summer >= 500: 
+  print("You can have a vacation in Europe! ")
+elif mark>= 80 and earnings_before_summer < 500:
+  print("You can have a vacation in California!")
+else: 
+  print("You can not have a vacation")
